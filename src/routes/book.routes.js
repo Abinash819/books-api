fc99c53fc43ca7a9dcb70375c367f86c8eef6fa8
Abinash Barik <myselfcoder819@gmail.com>
2026-02-03 00:04:10 +0530

    feat: add explore books with search filter sort and pagination

--- src/routes/book.routes.js
+++ src/routes/book.routes.js
@@ -2,5 +2,7 @@ const router = require("express").Router();
 const BookController = require("../controllers/book.controller");
 
 router.post("/", BookController.createBook);
+router.get("/", BookController.exploreBooks);
+
 
 module.exports = router;
